ff2cc6d0b40401c137624bf3735ec909c3dff6a0
shenef <matusik.rene@gmx.de>
2022-09-22 17:46:48 +0200

    make resetToMain.py executable from root

--- tools/mainMenuReset.py
+++
@@ -1,6 +0,0 @@
-import memory.main
-import reset
-
-memory.main.start()
-memory.main.clearEncounterID()
-reset.resetToMainMenu()
---
+++ tools/resetToMain.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+import memory.main
+import reset
+
+memory.main.start()
+memory.main.clearEncounterID()
+reset.resetToMainMenu()
