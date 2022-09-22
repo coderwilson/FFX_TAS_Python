3aa51ad28396332980d341fc5bf111e51ff179f8
shenef <matusik.rene@gmx.de>
2022-09-22 14:48:00 +0200

    remove unused imports

--- klikk.py
+++ klikk.py
@@ -1,33 +1,11 @@
 # Libraries and Core Files
-import logs
-import area.sin
-import area.zanarkand
-import area.gagazet
-import area.rescueYuna
-import area.home
-import area.mTemple
-import area.mWoods
-import area.thunderPlains
-import area.guadosalam
-import area.moonflow
-import area.djose
-import area.MRR
-import area.miihen
-import blitz
-import area.luca
-import area.kilika
-import area.boats
-import area.besaid
-import area.baaj
-import area.dreamZan
-import xbox
 import memory.main
-import battle.main
-import screen
-import vars
-import reset
+import xbox
+import area.dreamZan
 import loadGame
 import targetPathing
+import reset
+import logs
 gameVars = vars.varsHandle()
 gameVars.setStartVars()
 
@@ -39,7 +17,7 @@ area.dreamZan.NewGame("Klikk testing")
 loadGame.loadSaveNum(101)
 memory.main.awaitControl()
 while memory.main.userControl():
-    targetPathing.setMovement([0,0])
+    targetPathing.setMovement([0, 0])
     if memory.main.getCoords()[0] > -40:
         xbox.tapB()
 area.baaj.Klikk_fight()
