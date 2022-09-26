4d94d7552b8d0249cfa512019d5be307cccdf20c
coderwilson <84420541+coderwilson@users.noreply.github.com>
2022-09-26 12:56:10 -0700

    Most people will want to start from new game.

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ StepCounter = 3
 #StepCounter = 2 #Shedinja Highbridge
 #StepCounter = 3 #Before Sea of Sorrows
 #StepCounter = 4 #Before point of no return, with zombiestrike weapons (not Kimahri)
-#Gamestate = "none"
-#StepCounter = 1
+Gamestate = "none"
+StepCounter = 1
 
 # Nemesis load testing
 # Gamestate = "Nem_Farm"
