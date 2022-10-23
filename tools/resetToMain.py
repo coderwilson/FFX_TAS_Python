2d19804730a3b5047f8474ba8b3b4d814574bf31
shenef <matusik.rene@gmx.de>
2022-10-23 22:58:01 +0200

    convert files to LF line terminators

--- tools/resetToMain.py
+++ tools/resetToMain.py
@@ -1,12 +1,12 @@
 import inspect
 import os
 import sys
+import memory.main
+import reset
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-import memory.main
-import reset
 
 memory.main.start()
 memory.main.clear_encounter_id()
