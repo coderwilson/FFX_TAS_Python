1ceb809a451211c48cbc445295f52ebd91e56318
Jason Ward <jason.louard.ward@gmail.com>
2022-11-05 11:41:04 -0400

    Pulling naviagate from the correct place.

--- nemesis/arenaPrep.py
+++ nemesis/arenaPrep.py
@@ -37,7 +37,7 @@ def auto_life():
             xbox.tap_down()
     while not memory.main.other_battle_menu():
         xbox.tap_b()
-    memory.main._navigate_to_position(1)
+    battle.main._navigate_to_position(1)
     while memory.main.other_battle_menu():
         xbox.tap_b()
     xbox.tap_b()
