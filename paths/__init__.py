6782b2ef07d895971f4ab64e61a0e354b0e8ae31
orkaboy <orkaboy@gmail.com>
2022-11-10 22:23:00 +0100

    Baaj area pathing refactored

--- paths/__init__.py
+++ paths/__init__.py
@@ -1,3 +1,10 @@
+from paths.baaj import BaajHallway, BaajPuzzle, BaajRamp
 from paths.dream_zan import AllStartsHere, TidusHomeMovement
 
-__all__ = ["TidusHomeMovement", "AllStartsHere"]
+__all__ = [
+    "TidusHomeMovement",
+    "AllStartsHere",
+    "BaajRamp",
+    "BaajHallway",
+    "BaajPuzzle",
+]
