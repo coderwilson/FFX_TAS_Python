a3e1ffd1cbcd9152c3760f2af879527c2f554f01
shenef <matusik.rene@gmx.de>
2022-11-28 19:45:14 +0100

    fix errors in root_mem.py

--- root_mem.py
+++ root_mem.py
@@ -1,7 +1,17 @@
-import ctypes
-import ctypes.wintypes
-from ctypes import *
-from ctypes.wintypes import *
+import logging
+from ctypes import (
+    POINTER,
+    Structure,
+    addressof,
+    c_char,
+    c_void_p,
+    pointer,
+    sizeof,
+    windll,
+)
+from ctypes.wintypes import BYTE, DWORD, HMODULE
+
+logger = logging.getLogger(__name__)
 
 PROCESS_QUERY_INFORMATION = 0x0400
 PROCESS_VM_OPERATION = 0x0008
@@ -9,15 +19,15 @@ PROCESS_VM_READ = 0x0010
 PROCESS_VM_WRITE = 0x0020
 TH32CS_SNAPMODULE = 0x00000008
 
-CreateToolhelp32Snapshot = ctypes.windll.kernel32.CreateToolhelp32Snapshot
-Process32First = ctypes.windll.kernel32.Process32First
-Process32Next = ctypes.windll.kernel32.Process32Next
-Module32First = ctypes.windll.kernel32.Module32First
-Module32Next = ctypes.windll.kernel32.Module32Next
-GetLastError = ctypes.windll.kernel32.GetLastError
-OpenProcess = ctypes.windll.kernel32.OpenProcess
-GetPriorityClass = ctypes.windll.kernel32.GetPriorityClass
-CloseHandle = ctypes.windll.kernel32.CloseHandle
+CreateToolhelp32Snapshot = windll.kernel32.CreateToolhelp32Snapshot
+Process32First = windll.kernel32.Process32First
+Process32Next = windll.kernel32.Process32Next
+Module32First = windll.kernel32.Module32First
+Module32Next = windll.kernel32.Module32Next
+GetLastError = windll.kernel32.GetLastError
+OpenProcess = windll.kernel32.OpenProcess
+GetPriorityClass = windll.kernel32.GetPriorityClass
+CloseHandle = windll.kernel32.CloseHandle
 
 
 class MODULEENTRY32(Structure):
@@ -35,39 +45,6 @@ class MODULEENTRY32(Structure):
     ]
 
 
-def get_base_addr(ProcId, ProcName):
-    me32 = MODULEENTRY32()
-    me32.dwSize = sizeof(me32)
-    hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, ProcId)
-    if GetLastError() != 0:
-        CloseHandle(hSnapshot)
-        print("Handle Error %s", WinError())
-        return "Error"
-
-    else:
-        if Module32First(hSnapshot, byref(me32)):
-            if me32.szModule == ProcName:
-                CloseHandle(hSnapshot)
-                return id(me32.modBaseAddr)
-
-            else:
-                Module32Next(hSnapshot, byref(me32))
-                while int(GetLastError()) != 18:
-                    if me32.szModule == ProcName:
-                        CloseHandle(hSnapshot)
-                        return id(me32.modBaseAddr)
-
-                    else:
-                        Module32Next(hSnapshot, byref(me32))
-
-                CloseHandle(hSnapshot)
-                print("Couldn't find Process with name %s", ProcName)
-
-        else:
-            print("Module32First is False %s", WinError())
-            CloseHandle(hSnapshot)
-
-
 def list_process_modules(ProcessID):
     hModuleSnap = c_void_p(0)
     me32 = MODULEENTRY32()
@@ -76,27 +53,26 @@ def list_process_modules(ProcessID):
 
     ret = Module32First(hModuleSnap, pointer(me32))
     if ret == 0:
-        print("ListProcessModules() Error on Module32First[%d]", GetLastError())
+        logger.error("ListProcessModules() Error on Module32First[%d]", GetLastError())
         CloseHandle(hModuleSnap)
         return False
 
     complete = False
     while not complete:
-        print("MODULE NAME: %s", me32.szModule)
-        print("executable = %s", me32.szExePath)
-        print("process ID = 0x%08X", me32.th32ProcessID)
-        print("ref count (g) = 0x%04X", me32.GlblcntUsage)
-        print("ref count (p) = 0x%04X", me32.ProccntUsage)
-        print("base address = 0x%08X", me32.modBaseAddr)
+        logger.debug(f"MODULE NAME: {me32.szModule}")
+        logger.debug(f"executable: {me32.szExePath}")
+        logger.debug(f"process ID: {me32.th32ProcessID}")
+        logger.debug(f"ref count (g): {me32.GlblcntUsage}")
+        logger.debug(f"ref count (p): {me32.ProccntUsage}")
+        logger.debug(f"base address: {me32.modBaseAddr}")
         try:
-            print(
-                "Adjusted address = 0x%08X",
-                hex(ctypes.addressof(me32.modBaseAddr.contents)),
+            logger.debug(
+                f"Adjusted address = {hex(addressof(me32.modBaseAddr.contents))}"
             )
-            ret_val = ctypes.addressof(me32.modBaseAddr.contents)
+            ret_val = addressof(me32.modBaseAddr.contents)
         except Exception as x:
-            print("adjusted 3 error:", x)
-        print("base size = %d", me32.modBaseSize)
+            logger.debug(f"adjusted 3 error: {x}")
+        logger.debug(f"base size: {me32.modBaseSize}")
 
         if me32.szModule == b"FFX.exe":
             complete = True
