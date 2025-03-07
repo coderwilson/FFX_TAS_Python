4595961545c8930f2c926d82dc83ff4b0b4d023d
coderwilson <84420541+coderwilson@users.noreply.github.com>
2025-03-07 09:33:34 -0800

    Menu fixes in early game

--- v_coord_tracker.py
+++ v_coord_tracker.py
@@ -19,10 +19,10 @@ while map == get_map():
     current_time = int(time.time())
     if current_time != last_time:
         coords = get_actor_coords(actor_index=0)
-        print(f"Coords: [{round(coords[0], 2)}, {round(coords[1],2)}] | Angle: {round(angle,2)} | Dialog: {diag_progress_flag()}" + \
-            f" | Story: {get_story_progress()} | Map: {get_map()} | Skip: {memory.main.diag_skip_possible()}")
+        #print(f"Coords: [{round(coords[0], 2)}, {round(coords[1],2)}] | Angle: {round(angle,2)} | Dialog: {diag_progress_flag()}" + \
+        #    f" | Story: {get_story_progress()} | Map: {get_map()} | Skip: {memory.main.diag_skip_possible()}")
         #print(f"Battle Number: {get_encounter_id()}")
-        #print(f"Battle Number: {memory.main.s_grid_cursor_coords()}")
+        print(f"Battle Number: {memory.main.s_grid_cursor_coords()}")
         #memory.main.check_near_actors_print()
         last_time = current_time
 print(f"Map change, ({get_map()}) - ending program.")
